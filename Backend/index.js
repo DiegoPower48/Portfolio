75309f1116927e4101a70c2fdd807092882a0ad6
Diego <diego_torres_11@hotmail.com>
2024-06-07 06:30:08 -0500

    1

--- Backend/index.js
+++ Backend/index.js
@@ -6,9 +6,11 @@ var port = 27017;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost:27017/apirestportfolio").then(() => {
-  console.log("La conexión a la base de datos se ha realizado con bien!!");
-  app.listen(port, () => {
-    console.log("servidor corriendo en http://localhost:" + port);
+mongoose
+  .connect("mongodb://localhost:27017/apirestportfolio?authSource=admin")
+  .then(() => {
+    console.log("La conexión a la base de datos se ha realizado con bien!!");
+    app.listen(port, () => {
+      console.log("servidor corriendo en http://localhost:" + port);
+    });
   });
-});
