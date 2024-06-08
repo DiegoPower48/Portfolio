f12d5c4e26d54be5e24271ede208e711cbb78efe
Diego <diego_torres_11@hotmail.com>
2024-06-08 02:31:29 -0500

    ty

--- Backend/index.js
+++ Backend/index.js
@@ -6,9 +6,14 @@ var PORT = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost:27017/apirestportfolio").then(() => {
-  console.log("La conexión a la base de datos se ha realizado con bien!!");
-  app.listen(PORT, () => {
-    console.log("servidor corriendo en http://localhost:" + PORT);
+mongoose
+  .connect("mongodb://localhost:27017/apirestportfolio", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("La conexión a la base de datos se ha realizado con bien!!");
+    app.listen(PORT, () => {
+      console.log("servidor corriendo en http://localhost:" + PORT);
+    });
   });
-});
