029582d2930fe378bec1593b1e479d45350d3070
Diego <diego_torres_11@hotmail.com>
2024-06-08 03:38:30 -0500

    corrigiendo rutas 3

--- Backend/index.js
+++ Backend/index.js
@@ -30,7 +30,6 @@ mongoose
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // EJECUTAR XPRES
