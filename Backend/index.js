e49189bb305ead0f3850bae82a97652178aa1147
Diego <diego_torres_11@hotmail.com>
2024-06-08 06:13:29 -0500

    kjh

--- Backend/index.js
+++ Backend/index.js
@@ -30,6 +30,12 @@ mongoose
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
+const run = async () => {
+  await mongoose.connect("mongodb://127.0.0.1:27017/apirestportfolio");
+};
+
+run().catch((err) => console.error(err));
+
 app.listen(PORT, () => {
   console.log("servidor corriendo en http://localhost:" + PORT);
 });
