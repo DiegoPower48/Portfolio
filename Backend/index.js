d0049c7b2696f12bfae4794b776b1f8d309ced7a
Diego <diego_torres_11@hotmail.com>
2024-06-08 02:18:23 -0500

    asbs

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost:27017/apiresportfolio").then(() => {
+mongoose.connect("mongodb://localhost:27017/apirestportfolio").then(() => {
   console.log("La conexión a la base de datos se ha realizado con bien!!");
   app.listen(PORT, () => {
     console.log("servidor corriendo en http://localhost:" + PORT);
