99f50bb16d43ee19f9c9abfebb237933143fe693
Diego <diego_torres_11@hotmail.com>
2024-06-08 08:50:14 -0500

    prueba nueva

--- Backend/index.js
+++ Backend/index.js
@@ -20,6 +20,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/apirestportfolio")
