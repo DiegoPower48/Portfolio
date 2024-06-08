31395052396c219f2c9686254ed5b09664e2ca3e
Diego <diego_torres_11@hotmail.com>
2024-06-08 03:51:53 -0500

    cambiando host

--- Backend/index.js
+++ Backend/index.js
@@ -23,7 +23,7 @@ var PORT = process.env.PORT || 3000;
 
 // MongoDB connection
 mongoose
-  .connect("mongodb://127.0.0.1:27017/apirestportfolio", {
+  .connect("mongodb://localhost:27017/apirestportfolio", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
