365b062d234274ab2e47af20b96aa1f8001269fe
Diego <diego_torres_11@hotmail.com>
2024-06-08 05:09:34 -0500

    corrigiendo Backend5

--- Backend/index.js
+++ Backend/index.js
@@ -20,15 +20,19 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
 // MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/apirestportfolio").then(() => {
-  console.log("La conexión a la base de datos se ha realizado con bien!!");
-  app.listen(PORT, () => {
-    console.log("servidor corriendo en http://localhost:" + PORT);
-  });
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/apirestportfolio")
+  .then(() => {
+    console.log("La conexión a la base de datos se ha realizado con bien!!");
+  })
+  .catch((err) => console.log(err));
 
 // EJECUTAR XPRES
 const app = express();
+
+app.listen(PORT, () => {
+  console.log("servidor corriendo en http://localhost:" + PORT);
+});
 
 //CARGAR RUTAS
 const solicitudRutas = require("./routes/solicitud");
