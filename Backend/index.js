d90017d30ece9478ff410c290d096e88249e343e
Diego <diego_torres_11@hotmail.com>
2024-06-08 07:43:40 -0500

    cors nuevamente

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Reemplaza con tu dominio
+  origin: "https://diego-dev-portfolio.vercel.app", // Reemplaza con tu dominio
   optionsSuccessStatus: 200,
 };
 
