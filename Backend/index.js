6689feef04d60df4d65f6d35054d68a65319d62d
Diego <diego_torres_11@hotmail.com>
2024-06-08 02:05:35 -0500

    Backend

--- Backend/index.js
+++ Backend/index.js
@@ -2,13 +2,13 @@
 
 var mongoose = require("mongoose");
 var app = require("./app");
-var port = 3000;
+var PORT = process.env.PORT || 3000;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost:27017/apirestportfolio").then(() => {
+mongoose.connect("mongodb://localhost:27017/apiresportfolio").then(() => {
   console.log("La conexión a la base de datos se ha realizado con bien!!");
-  app.listen(port, () => {
-    console.log("servidor corriendo en http://localhost:" + port);
+  app.listen(PORT, () => {
+    console.log("servidor corriendo en http://localhost:" + PORT);
   });
 });
