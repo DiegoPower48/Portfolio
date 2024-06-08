e76a2faaf11d2d1cdf7917d59320eb156bda0f90
Diego <diego_torres_11@hotmail.com>
2024-06-08 03:04:30 -0500

    abhcd

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,40 @@
 "use strict";
 
-var mongoose = require("mongoose");
-var app = require("./app");
-var PORT = process.env.PORT || 3000;
+// var mongoose = require("mongoose");
+// var app = require("./app");
+// var PORT = process.env.PORT || 3000;
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://127.0.0.1:27017/apirestportfolio").then(() => {
-  console.log("La conexión a la base de datos se ha realizado con bien!!");
-  app.listen(PORT, () => {
-    console.log("servidor corriendo en http://localhost:" + PORT);
-  });
-});
+// mongoose.connect("mongodb://127.0.0.1:27017/apirestportfolio").then(() => {
+//   console.log("La conexión a la base de datos se ha realizado con bien!!");
+//   app.listen(PORT, () => {
+//     console.log("servidor corriendo en http://localhost:" + PORT);
+//   });
+// });
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+
+// Middlewares
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/apirestportfolio", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+// Routes
+const usersRoute = require("./routes/users");
+app.use("/api/users", usersRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
