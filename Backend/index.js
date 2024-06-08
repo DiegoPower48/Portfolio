dbc03d76534e0bd743aa08b0b7faa2662f7e98e7
Diego <diego_torres_11@hotmail.com>
2024-06-08 07:12:38 -0500

    abcd

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,5 @@
 "use strict";
 
-// var mongoose = require("mongoose");
-// var app = require("./app");
-// var PORT = process.env.PORT || 3000;
-
-// mongoose.Promise = global.Promise;
-
 // mongoose.connect("mongodb://127.0.0.1:27017/apirestportfolio").then(() => {
 //   console.log("La conexión a la base de datos se ha realizado con bien!!");
 //   app.listen(PORT, () => {
