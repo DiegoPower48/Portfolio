f633ab10cd246a935c1fc19b85bd17c533840110
Diego <diego_torres_11@hotmail.com>
2024-06-08 20:33:19 -0500

    cancel deploy

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/apirestportfolio")
