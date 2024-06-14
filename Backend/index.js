79cfbb12065b1150d0bf09ce6b6c582558c98f00
Diego <diego_torres_11@hotmail.com>
2024-06-13 22:14:05 -0500

    bac

--- Backend/index.js
+++ Backend/index.js
@@ -4,15 +4,42 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Lista de orígenes permitidos
+      const ACCEPTED_ORIGINS = [
+        "http://localhost:5173",
+        "https://portfolio-8az3.onrender.com",
+      ];
+      // Permitir solicitudes sin origen (como curl o Postman)
+      if (!origin) return callback(null, true);
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
+
 // Conectar a MongoDB
-mongoose.connect(process.env.MONGODB_URI);
-console.log("Conectado a MongoDB");
+mongoose.connect(
+  "mongodb+srv://diegotorres11:aCljStNR9of8uZhi@portfoliosolicitudes.zim6muz.mongodb.net/"
+);
+mongoose.connection.on("connected", () => {
+  console.log("Conectado a MongoDB");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error al conectar a MongoDB: ", err);
+});
 
 // Definir un modelo
 const itemSchema = new mongoose.Schema(
@@ -29,16 +56,17 @@ const Item = mongoose.model("Item", itemSchema);
 // Rutas
 
 const ACCEPTED_ORIGINS = [
-  "http://localhost:5173/",
+  "http://localhost:5173",
   "https://portfolio-8az3.onrender.com/correo",
   "https://portfolio-8az3.onrender.com",
 ];
 
 app.post("/correo", async (req, res) => {
-  const origin = req.header("origin");
-  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
+  // const origin = req.headers.origin;
+  // if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+  //   res.header("Access-Control-Allow-Origin", origin);
+  // }
+
   try {
     const item = new Item({
       nombre: req.body.nombre,
@@ -52,14 +80,14 @@ app.post("/correo", async (req, res) => {
   }
 });
 
-app.options("/correo", (req, res) => {
-  const origin = req.header("origin");
-  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
-    res.header("Access-Control-Allow-Origin", origin);
-    res.header("Access-Control-Allow-Methods", "POST,OPTIONS");
-  }
-  res.status(200);
-});
+// app.options("/correo", (req, res) => {
+//   const origin = req.headers.origin;
+//   if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+//     res.header("Access-Control-Allow-Origin", origin);
+//     res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
+//   }
+//   res.sendStatus(200);
+// });
 
 // Iniciar el servidor
 app.listen(port, () => {
