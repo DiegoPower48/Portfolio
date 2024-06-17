759f1ec952fada92e1a528118719a8db419aefa2
Diego <diego_torres_11@hotmail.com>
2024-06-17 08:07:35 -0500

    AÑADIENDO SECCION PROYECTOS Y SU RESPONSIVE

--- portfolio/src/Components/Proyectos/index.tsx
+++ portfolio/src/Components/Proyectos/index.tsx
@@ -2,8 +2,44 @@ function Proyectos() {
   return (
     <div className="proyectos" id="Proyectos">
       <h1>Proyectos:</h1>
+
+      <Trabajos />
     </div>
   );
 }
 
+function Trabajos() {
+  const proyectos = [
+    {
+      nombre: "aplicación vacia",
+      imagen: "https://www.svgrepo.com/show/512317/github-142.svg",
+      direccion: "https://github.com/DiegoPower48",
+      descripcion:
+        "Proyecto creado utilizando React, NodeJS, Express, conectado a una api",
+    },
+  ];
+
+  return (
+    <>
+      {proyectos.map((element, index) => (
+        <>
+          <div className="cajaProyectos" key={index}>
+            <a href={element.direccion}>
+              <p className="tituloProyectos">{element.nombre}</p>
+            </a>
+
+            <a href={element.direccion} target="_blank" className="cajaimagen">
+              <img
+                src={element.imagen}
+                className="imagenProyectos"
+                alt="github-icon"
+              />
+            </a>
+            <p className="descripcionProyecto">{proyectos[0].descripcion}</p>
+          </div>
+        </>
+      ))}
+    </>
+  );
+}
 export default Proyectos;
