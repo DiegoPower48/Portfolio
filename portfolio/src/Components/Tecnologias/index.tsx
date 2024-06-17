9a92fa1168c126dfd10745202a2d2e5b6c4d87a8
Diego <diego_torres_11@hotmail.com>
2024-06-17 03:51:43 -0500

    animacion de iconos

--- portfolio/src/Components/Tecnologias/index.tsx
+++ portfolio/src/Components/Tecnologias/index.tsx
@@ -18,52 +18,52 @@ export function Iconos() {
   return (
     <>
       <img
-        className="Iconos"
+        className="Iconos Icono1"
         src="https://www.svgrepo.com/show/373669/html.svg"
         alt="html"
       />
       <img
-        className="Iconos"
+        className="Iconos Icono2"
         src="https://www.svgrepo.com/show/452185/css-3.svg"
       />
       <img
-        className="Iconos"
+        className="Iconos Icono3"
         src="https://www.svgrepo.com/show/353498/bootstrap.svg"
         alt="bootstrap-logo"
       />
 
       <img
-        className="Iconos"
+        className="Iconos Icono4"
         src="https://www.svgrepo.com/show/349419/javascript.svg"
         alt="javascript-logo"
       />
       <img
-        className="Iconos"
+        className="Iconos Icono5"
         src="https://www.svgrepo.com/show/439290/react.svg"
         alt="react-logo"
       />
       <img
-        className="Iconos"
+        className="Iconos Icono6"
         src="https://www.svgrepo.com/show/452091/python.svg"
         alt="python-logo"
       />
       <img
-        className="Iconos"
+        className="Iconos Icono7"
         src="https://www.svgrepo.com/show/452156/angular.svg"
         alt="angular-logo"
       />
       <img
-        className="Iconos"
+        className="Iconos Icono8"
         src="https://www.svgrepo.com/show/374146/typescript-official.svg"
         alt="typescript-logo"
       />
       <img
-        className="Iconos"
+        className="Iconos Icono9"
         src="https://www.svgrepo.com/show/331761/sql-database-sql-azure.svg"
         alt="sql-logo"
       />
       <img
-        className="Iconos"
+        className="Iconos Icono10"
         src="https://www.svgrepo.com/show/331488/mongodb.svg"
         alt="mongodb-logo"
       />
