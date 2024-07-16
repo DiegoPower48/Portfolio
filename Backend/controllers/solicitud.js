917e3142c0288c69eac6623fd27121bc9877e318
Diego <diego_torres_11@hotmail.com>
2024-07-16 05:10:39 -0500

    terminando las cookies

--- Backend/controllers/solicitud.js
+++ Backend/controllers/solicitud.js
@@ -57,10 +57,6 @@ const controller = {
       res.cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000, // 1 día de duración
         httpOnly: true,
-        domain:
-          process.env.NODE_ENV === "development"
-            ? "http://localhost:5173"
-            : "https://chatportfolio-production-c9b8.up.railway.app",
         secure: process.env.NODE_ENV === "production", // true en producción
         sameSite: "None", // None para permitir cookies entre sitios
       });
@@ -90,11 +86,6 @@ const controller = {
       await res.cookie("token", token, {
         maxAge: 24 * 60 * 60 * 1000, // 1 día de duración
         httpOnly: true,
-        domain:
-          process.env.NODE_ENV === "development"
-            ? "http://localhost:5173"
-            : "https://chatportfolio-production-c9b8.up.railway.app",
-
         secure: process.env.NODE_ENV === "production", // true en producción
         sameSite: "None", // None para permitir cookies entre sitios
       });
