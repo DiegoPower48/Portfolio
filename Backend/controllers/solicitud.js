15524abe6cfe70a74d5d189d50c5f10cd9779d87
Diego <diego_torres_11@hotmail.com>
2024-07-19 22:24:24 -0500

    probando domain

--- Backend/controllers/solicitud.js
+++ Backend/controllers/solicitud.js
@@ -59,7 +59,7 @@ const controller = {
         secure: true,
         sameSite: "None",
         maxAge: 1000 * 60 * 60 * 24, // 1 día
-        domain: "https://backendportfolio-one.vercel.app", // Ajusta esto a tu dominio
+        domain: "https://chatportfolio.vercel.app/chat", // Ajusta esto a tu dominio
       });
       res.json({ usuarioguardado });
     } catch (err) {
@@ -89,7 +89,7 @@ const controller = {
         secure: true,
         sameSite: "None",
         maxAge: 1000 * 60 * 60 * 24, // 1 día
-        domain: "https://backendportfolio-one.vercel.app", // Ajusta esto a tu dominio
+        domain: "https://chatportfolio.vercel.app/chat", // Ajusta esto a tu dominio
       });
       console.log("despues de setear cookies");
       res.send("login exitoso");
