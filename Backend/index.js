f1f54c971b5bd076f38889154cad62cef951c7ae
Diego <diego_torres_11@hotmail.com>
2024-11-04 02:18:03 -0500

    eliminando ruta no usada

--- Backend/index.js
+++ Backend/index.js
@@ -32,7 +32,6 @@ app.use(
       "https://teddy-store.vercel.app",
       "https://diegotorres-portfoliodev.vercel.app",
       "https://chatportfolio.vercel.app",
-      "exp://192.168.18.169:8081",
       "https://daysi-block.vercel.app",
     ],
     methods: ["GET", "PUT", "POST", "OPTIONS", "DELETE"],
@@ -49,11 +48,7 @@ app.use(store);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://chatportfolio.vercel.app",
-      "exp://192.168.18.169:8081",
-    ],
+    origin: ["http://localhost:5173", "https://chatportfolio.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
