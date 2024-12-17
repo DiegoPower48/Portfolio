f5df3adc2d5af555aada0cd261813927c2385905
Diego <diego_torres_11@hotmail.com>
2024-12-17 01:56:50 -0500

    editando valores requeridos al registro

--- Backend/middelwares/validator.js
+++ Backend/middelwares/validator.js
@@ -5,6 +5,7 @@ const validateSchema = (schema) => (req, res, next) => {
     schema.parse(req.body);
     next();
   } catch (error) {
+    console.log(error);
     return res.status(400).json(error.errors.map((error) => error.message));
   }
 };
--- Backend/models/appvalidation.js
+++ Backend/models/appvalidation.js
@@ -3,9 +3,14 @@
 const { z } = require("zod");
 
 const registerSchema = z.object({
-  correo: z.string().refine((correo) => correo.endsWith("@utp.edu.pe"), {
-    message: "El correo ingresado no es un correo institucional",
-  }),
+  correo: z
+    .string()
+    .refine((correo) => correo.startsWith("u") || correo.startsWith("U"), {
+      message: "El correo ingresado no es un correo institucional",
+    })
+    .refine((correo) => correo.endsWith("@utp.edu.pe"), {
+      message: "El correo ingresado no es un correo institucional",
+    }),
 });
 
 module.exports = {
